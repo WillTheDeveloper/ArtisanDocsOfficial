cdae16a4849406cccf379841cdceb311cce83d41
Will <will@teamatlas.cloud>
2021-08-12 16:10:20 +0100

    Created more components to enclose all the main headers on each page

---
+++ app/Http/Livewire/LivewireHeader.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class LivewireHeader extends Component
+{
+    public function render()
+    {
+        return view('livewire.livewire-header');
+    }
+}
---
+++ app/Http/Livewire/TailwindHeader.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class TailwindHeader extends Component
+{
+    public function render()
+    {
+        return view('livewire.tailwind-header');
+    }
+}
