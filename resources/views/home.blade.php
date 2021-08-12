09a90a9f3b344fc8e39ae5a17fdc6cfa36b27754
Will <will@teamatlas.cloud>
2021-08-12 14:17:15 +0100

    Updated links on home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -125,11 +125,11 @@
                     <span class="block text-amber-500 xl:inline">Docs</span>
                 </h1>
                 <p class="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
-                    A place where you can get easy to follow documentation for everything you need to know to create an awesome web based platform.
+                    A place where you can get easy to follow documentation for everything you need to know to create an awesome web based platform. <br> All the following docs are unofficial and were written by me.
                 </p>
                 <div class="mt-5 max-w-md mx-auto sm:flex sm:justify-center md:mt-8">
                     <div class="rounded-md shadow">
-                        <a href="#" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-amber-500 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10">
+                        <a href="/laravel" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-amber-500 hover:bg-amber-400 md:py-4 md:text-lg md:px-10">
                             Get Started!
                         </a>
                     </div>
