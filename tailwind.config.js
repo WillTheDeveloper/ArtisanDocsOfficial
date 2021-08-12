c188d362a6b7164fa2875cceba4e37744038c7ae
Will <will@teamatlas.cloud>
2021-08-12 14:04:20 +0100

    Adjusted colour on support page

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,7 @@ module.exports = {
             indigo: colors.indigo,
             white: colors.white,
             amber: colors.amber,
+            emerald: colors.emerald,
         },
         extend: {},
     },
