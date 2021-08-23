003c9793873fab05c0eec31b5c729c607e5a54c2
Will <will@teamatlas.cloud>
2021-08-23 22:02:11 +0100

    Added banner to coming soon page

--- resources/views/livewire/coming-soon.blade.php
+++ resources/views/livewire/coming-soon.blade.php
@@ -1,4 +1,5 @@
 <div>
+    @livewire('development-banner')
     <!-- This example requires Tailwind CSS v2.0+ -->
     <div class="bg-white min-h-screen px-4 py-16 sm:px-6 sm:py-24 md:grid md:place-items-center lg:px-8">
         <div class="max-w-max mx-auto">
